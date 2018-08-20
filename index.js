c4772af0eaa3bb9d2d242be57277d3cb8518ad97
HWAN OH <hwangoon@gmail.com>
2018-08-21 00:43:29 +0900

    init

---
+++ index.js
@@ -0,0 +1,66 @@
+const TelegramBot = require('node-telegram-bot-api');
+var os = require('os');
+require('dotenv').config();
+ 
+// replace the value below with the Telegram token you receive from @BotFather
+const token = process.env.TELEGRAM_API_KEY;
+ 
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+
+// Matches "/start [whatever]"
+bot.onText(/\/start/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(
+    chatId,
+    "LAB543 테스트봇에 오신 것을 환영합니다." + os.EOL
+    + "도움말을 보시려면 /help 를 입력해주세요."
+  );
+});
+
+// Matches "/start [whatever]"
+bot.onText(/\/help/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(
+    chatId,
+    "/help: 이 페이지 표시" + os.EOL
+    + "/echo [메세지]: 메세지 echo"
+  );
+});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+ 
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+ 
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+ 
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+ 
+  // send a message to the chat acknowledging receipt of their message
+  //bot.sendMessage(chatId, 'Received your message');
+  //bot.sendMessage(chatId, 'echo: '+msg.text);
+  console.log(msg);
+});
